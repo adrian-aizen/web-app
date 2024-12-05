7f76ece04674b48f6cad7e69c0ae58cf77d65d51
Adrian Adora <aidss24adora@gmail.com>
2024-12-05 20:23:46 +0800

    minor changes

--- assets/Controller.js
+++ assets/Controller.js
@@ -1,6 +1,5 @@
 const item = require('./movieDB/movieDB.js'); 
 
-// Get all items from the database
 exports.getMovie = (req, res) => {
     item.getMovie((err, movies) => {
         if (err) {
@@ -10,27 +9,26 @@ exports.getMovie = (req, res) => {
     });
 };
 
-// Add a new item to the database
-exports.addMovie = (req, res) => {
-    console.log('Request body:', req.body); // Log the request body
-    const { movie_name, movie_genre, movie_duration, movie_description } = req.body;
 
-    //validation
+exports.addMovie = (req, res) => {
+    console.log('Request body:', req.body); 
+    const { movie_name, movie_genre, movie_duration, movie_description } = req.body;
+    
     if (!movie_name || !movie_genre || !movie_duration || !movie_description) {
         return res.status(400).send('All fields are required.');
     }
 
     const duration = parseInt(movie_duration, 10);
-    if (isNaN(quantityNumber)) {
-        return res.status(400).send('Quantity must be a number.');
+    if (isNaN(movie_duration)) {
+        return res.status(400).send('Duration must be a number.');
     }
 
     item.addMovie(movie_name, movie_genre, duration, movie_description, (err, item) => {
         if (err) {
-            console.error("Error adding item:", err.message);
+            console.error("Error adding movie:", err.message);
             return res.status(500).send(err.message);
         }
-        console.log("Item added successfully:", movies); 
+        console.log("Movie added successfully:", movies); 
         res.status(201).json(movies);
     });
 };
