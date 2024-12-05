d52a7de63c24af8843810715298399b70bf664a2
Adrian Adora <aidss24adora@gmail.com>
2024-12-05 20:05:34 +0800

    added functions for database

--- assets/movieDB/movieDB.js
+++ assets/movieDB/movieDB.js
@@ -5,10 +5,10 @@ const db = new sqlite3.Database('./movieDB.db', (err) => {
     return console.error("Error opening database! ", err.message)
   }else{
     console.log("Successfully connected to the database!");
-    db.run(`CREATE TABLE IF NOT EXISTS movies (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    db.run(`CREATE TABLE IF NOT EXISTS movies (movie_id INTEGER PRIMARY KEY AUTOINCREMENT, 
       movie_name TEXT, 
       movie_genre TEXT, 
-      movie_duration TIME, 
+      movie_duration INT, 
       movie_description TEXT)` ,(err) => {
       if (err) {
         return console.error("Error creating table! ", err.message)
@@ -18,5 +18,31 @@ const db = new sqlite3.Database('./movieDB.db', (err) => {
     })
   }
 });
+
+const getMovie = (callback) => {
+  db.all(`SELECT * FROM movies`, [], (err, rows) => {
+      if (err) {
+          return callback(err);
+      }
+      callback(null, rows);
+  });
+};
+
+const addMovie = (movie_name, movie_genre, movie_duration, movie_description, callback) => {
+  const sql = `INSERT INTO movies (movie_name, movie_genre, movie_duration, movie_description) VALUES (?, ?, ?, ?)`;
+  db.run(sql, [movie_name, movie_genre, movie_duration, movie_description], function(err) {
+      if (err) {
+          console.error("Error inserting movie: ", err.message); 
+          return callback(err);
+      }
+      console.log("Inserted movie with ID:", this.lastID);
+      callback(null, { movie_id: this.lastID, movie_name, movie_genre, movie_duration, movie_description });
+  });
+};
+
+module.exports = {
+  getMovie,
+  addMovie
+};
 
 module.exports = db;
